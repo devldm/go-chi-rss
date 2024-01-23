a6d743becac0035f472628647f78fd008cd2bc87
liam <39243060+devldm@users.noreply.github.com>
2024-01-23 20:39:46 +0900

    remove commented code

--- middleware/api_config.go
+++ middleware/api_config.go
@@ -7,14 +7,6 @@ import (
 	"github.com/devldm/go-server-rss/db"
 )
 
-// func ConfigMiddleware(next http.Handler, config *db.APIConfig) http.Handler {
-// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		ctx := context.WithValue(r.Context(), "api_config", config)
-
-// 		next.ServeHTTP(w, r.WithContext(ctx))
-// 	})
-// }
-
 func ConfigMiddleware(config *db.APIConfig) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
